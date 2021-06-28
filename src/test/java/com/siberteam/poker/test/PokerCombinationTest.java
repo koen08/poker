c6ce0b527c419990aab90aa05c9da61e286a07e0
koen <konstantin.butusov2017@yandex.ru>
2021-06-28 16:02:40 +0700

    refactor tests. Delete try/catch

--- src/test/java/com/siberteam/poker/test/PokerCombinationTest.java
+++ src/test/java/com/siberteam/poker/test/PokerCombinationTest.java
@@ -10,113 +10,73 @@ import static org.junit.Assert.assertEquals;
 public class PokerCombinationTest {
 
     @Test
-    public void isHighCardCombination() {
+    public void isHighCardCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("2C 5S 6D QD TD");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.HIGH_CARD.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("2C 5S 6D QD TD");
+        assertEquals(PokerCombinations.HIGH_CARD.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isPairCombination() {
+    public void isPairCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("2C 2S 6D QD TD");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.PAIR.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("2C 2S 6D QD TD");
+        assertEquals(PokerCombinations.PAIR.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isTwoPairCombination() {
+    public void isTwoPairCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("2C 2S QC QD TD");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.TWO_PAIR.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("2C 2S QC QD TD");
+        assertEquals(PokerCombinations.TWO_PAIR.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isThreePairCombination() {
+    public void isThreePairCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("2C 2S 2H QD TD");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.THREE_PAIR.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("2C 2S 2H QD TD");
+        assertEquals(PokerCombinations.THREE_PAIR.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isStraightCombination() {
+    public void isStraightCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("5C 6S 7H 8D 9D");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.STRAIGHT.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("5C 6S 7H 8D 9D");
+        assertEquals(PokerCombinations.STRAIGHT.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isFlushCombination() {
+    public void isFlushCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("5C QC 6C 8C TC");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.FLUSH.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("5C QC 6C 8C TC");
+        assertEquals(PokerCombinations.FLUSH.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isFullHouseCombination() {
+    public void isFullHouseCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("5C 5D 5S 8C 8H");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.FULL_HOUSE.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("5C 5D 5S 8C 8H");
+        assertEquals(PokerCombinations.FULL_HOUSE.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isFourPairCombination() {
+    public void isFourPairCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("5C 5S 5D 5H TC");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.FOUR_PAIR.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("5C 5S 5D 5H TC");
+        assertEquals(PokerCombinations.FOUR_PAIR.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isStraightFlushCombination() {
+    public void isStraightFlushCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("5C 6C 7C 8C 9C");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.STRAIGHT_FLUSH.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("5C 6C 7C 8C 9C");
+        assertEquals(PokerCombinations.STRAIGHT_FLUSH.name(), pokerHand.getNamePokerCombination());
     }
 
     @Test
-    public void isRoyalFlushCombination() {
+    public void isRoyalFlushCombination() throws PokerHandException {
         PokerHand pokerHand = null;
-        try {
-            pokerHand = new PokerHand("TC QC JC AC KC");
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
-        assertEquals(PokerCombinations.ROYAL_FLUSH.name(), pokerHand != null ? pokerHand.getNamePokerCombination() : null);
+        pokerHand = new PokerHand("TC QC JC AC KC");
+        assertEquals(PokerCombinations.ROYAL_FLUSH.name(), pokerHand.getNamePokerCombination());
     }
 
 }
--- src/test/java/com/siberteam/poker/test/PokerSortTest.java
+++ src/test/java/com/siberteam/poker/test/PokerSortTest.java
@@ -13,236 +13,164 @@ import static org.junit.Assert.assertEquals;
 public class PokerSortTest {
 
     @Test
-    public void isMorePairThenHighCard() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2C 5S 6D QD TD"));
-            pokerHandList.add(new PokerHand("2C 2S 6D QD TD"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMorePairThenHighCard() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2C 5S 6D QD TD"));
+        pokerHandList.add(new PokerHand("2C 2S 6D QD TD"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreTwoPairThenPair() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2D 2H 6D QD QC"));
-            pokerHandList.add(new PokerHand("2C 2S 6D QD TD"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.TWO_PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreTwoPairThenPair() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2D 2H 6D QD QC"));
+        pokerHandList.add(new PokerHand("2C 2S 6D QD TD"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.TWO_PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreThreePairThenTwoPair() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2D 2H 2D 6D QC"));
-            pokerHandList.add(new PokerHand("2C 2S 6D QD QS"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.THREE_PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreThreePairThenTwoPair() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2D 2H 2D 6D QC"));
+        pokerHandList.add(new PokerHand("2C 2S 6D QD QS"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.THREE_PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreStraightThenThreePair() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2D 3H 4D 5D 6C"));
-            pokerHandList.add(new PokerHand("2C 2S 2D TD QS"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.STRAIGHT.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreStraightThenThreePair() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2D 3H 4D 5D 6C"));
+        pokerHandList.add(new PokerHand("2C 2S 2D TD QS"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.STRAIGHT.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreFlushThenStraight() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2D 3H 4D 5D 6C"));
-            pokerHandList.add(new PokerHand("2C 6C 3C QC TC"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.FLUSH.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreFlushThenStraight() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2D 3H 4D 5D 6C"));
+        pokerHandList.add(new PokerHand("2C 6C 3C QC TC"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.FLUSH.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreFullHouseThenFlush() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2C 6C 3C QC TC"));
-            pokerHandList.add(new PokerHand("2C 2S 2D QD QS"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.FULL_HOUSE.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreFullHouseThenFlush() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2C 6C 3C QC TC"));
+        pokerHandList.add(new PokerHand("2C 2S 2D QD QS"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.FULL_HOUSE.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreFourPairThenFullHouse() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2C 2S 2D 2C TC"));
-            pokerHandList.add(new PokerHand("2C 2S 2D QD QS"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.FOUR_PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreFourPairThenFullHouse() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2C 2S 2D 2C TC"));
+        pokerHandList.add(new PokerHand("2C 2S 2D QD QS"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.FOUR_PAIR.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreStraightFlushThenFourPair() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("2C 2S 2D 2C TC"));
-            pokerHandList.add(new PokerHand("2C 3C 4C 5C 6C"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.STRAIGHT_FLUSH.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreStraightFlushThenFourPair() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("2C 2S 2D 2C TC"));
+        pokerHandList.add(new PokerHand("2C 3C 4C 5C 6C"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.STRAIGHT_FLUSH.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void isMoreRoyalFlushThenStraightFlush() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("TC JC QC KC AC"));
-            pokerHandList.add(new PokerHand("2C 3C 4C 5C 6C"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals(PokerCombinations.ROYAL_FLUSH.name(), pokerHandList.get(1).getNamePokerCombination());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void isMoreRoyalFlushThenStraightFlush() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("TC JC QC KC AC"));
+        pokerHandList.add(new PokerHand("2C 3C 4C 5C 6C"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals(PokerCombinations.ROYAL_FLUSH.name(), pokerHandList.get(1).getNamePokerCombination());
     }
 
     @Test
-    public void oneHighCardStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("AH 3C 7C TC 6C"));
-            pokerHandList.add(new PokerHand("3C 2D QC TH 7H"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("AH 3C 7C TC 6C", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneHighCardStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("AH 3C 7C TC 6C"));
+        pokerHandList.add(new PokerHand("3C 2D QC TH 7H"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("AH 3C 7C TC 6C", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void onePairStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("AH AC 7C TC 6C"));
-            pokerHandList.add(new PokerHand("3C 3D QC TH 7H"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("AH AC 7C TC 6C", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void onePairStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("AH AC 7C TC 6C"));
+        pokerHandList.add(new PokerHand("3C 3D QC TH 7H"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("AH AC 7C TC 6C", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneTwoPairStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("AH AC 7C TC 6C"));
-            pokerHandList.add(new PokerHand("3C 3D QC TH 7H"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("AH AC 7C TC 6C", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneTwoPairStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("AH AC 7C TC 6C"));
+        pokerHandList.add(new PokerHand("3C 3D QC TH 7H"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("AH AC 7C TC 6C", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneThreePairStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("AH AC AD TC 6C"));
-            pokerHandList.add(new PokerHand("3C 3D 3H TH 7H"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("AH AC AD TC 6C", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneThreePairStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("AH AC AD TC 6C"));
+        pokerHandList.add(new PokerHand("3C 3D 3H TH 7H"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("AH AC AD TC 6C", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneStraightStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("3D 4S 5H 6H 7H"));
-            pokerHandList.add(new PokerHand("2H 3C 4D 5C 6C"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("3D 4S 5H 6H 7H", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneStraightStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("3D 4S 5H 6H 7H"));
+        pokerHandList.add(new PokerHand("2H 3C 4D 5C 6C"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("3D 4S 5H 6H 7H", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneFlushStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("3D 4D 5D 6D 7D"));
-            pokerHandList.add(new PokerHand("2D 3D 4D 5D 6D"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("3D 4D 5D 6D 7D", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneFlushStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("3D 4D 5D 6D 7D"));
+        pokerHandList.add(new PokerHand("2D 3D 4D 5D 6D"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("3D 4D 5D 6D 7D", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneFullHouseStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("3S 3H 6S 6D 6H"));
-            pokerHandList.add(new PokerHand("3D 3S 3H 6D 6S"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("3S 3H 6S 6D 6H", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneFullHouseStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("3S 3H 6S 6D 6H"));
+        pokerHandList.add(new PokerHand("3D 3S 3H 6D 6S"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("3S 3H 6S 6D 6H", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneFourPairStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("3S 3H 3C 3D 6H"));
-            pokerHandList.add(new PokerHand("2D 2S 2H 2D 6S"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("3S 3H 3C 3D 6H", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneFourPairStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("3S 3H 3C 3D 6H"));
+        pokerHandList.add(new PokerHand("2D 2S 2H 2D 6S"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("3S 3H 3C 3D 6H", pokerHandList.get(1).getPokerHandInput());
     }
 
     @Test
-    public void oneStraightFlushStrongerThanOther() {
-        try {
-            List<PokerHand> pokerHandList = new ArrayList<>();
-            pokerHandList.add(new PokerHand("3S 4S 5S 6S 7S"));
-            pokerHandList.add(new PokerHand("2D 3D 4D 5D 6D"));
-            pokerHandList.sort(PokerHand.pokerHandComparator);
-            assertEquals("3S 4S 5S 6S 7S", pokerHandList.get(1).getPokerHandInput());
-        } catch (PokerHandException e) {
-            e.printStackTrace();
-        }
+    public void oneStraightFlushStrongerThanOther() throws PokerHandException {
+        List<PokerHand> pokerHandList = new ArrayList<>();
+        pokerHandList.add(new PokerHand("3S 4S 5S 6S 7S"));
+        pokerHandList.add(new PokerHand("2D 3D 4D 5D 6D"));
+        pokerHandList.sort(PokerHand.pokerHandComparator);
+        assertEquals("3S 4S 5S 6S 7S", pokerHandList.get(1).getPokerHandInput());
     }
 }
